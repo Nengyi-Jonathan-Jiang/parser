71b4135c0fcf790e15c90cb821c7734822dad337
Nengyi Jonathan Jiang <k1334755@students.katyisd.org>
2022-05-07 14:48:35 -0500

    .

--- Main.java
+++ Main.java
@@ -1,30 +1,9 @@
-import java.util.*;
-import java.util.stream.*;
-
-import compiler.grammar.Grammar;
-import compiler.grammar.GrammarReader;
-import compiler.items.Item;
 import compiler.parsers.*;
-import compiler.*;
 
 public class Main {
 	public static void main(String[] args) {
-//		System.out.println("Generating Grammar...");
-//
-//		Grammar gram = GrammarReader.readFromFile("Code.ebnf");
-//
-//		System.out.println("===================");
-//		System.out.println(gram);
-//		System.out.println("===================");
-
 		System.out.println("Generating Parser...");
-
-		// Parser parse = new LR0Parser(gram);
-		// Parser parse = new SLRParser(gram);
-//		LRParser parse = new LR1Parser(gram);
 		LRParser parse = new LRParserFromFile("Code.ptbl");
-
-//		parse.saveParsingTableToFile("Code.ptbl");
 
 		System.out.println("Parsing...");
 
@@ -33,9 +12,5 @@ public class Main {
 		ParseTree pTree = parse.parse(str.split(" "), true);
 		if(pTree == null) System.out.println("ERROR PARSING STRING");
 		else System.out.println(pTree.prnt());
-	}
-	
-	public static String prnt(Collection<?> c){
-	    return c.stream().map(i->"\"" + i + "\"").collect(Collectors.toList()).toString();
 	}
 }
