de95ea87a1cf51fa3d6c3991f2e7512000b9aae3
Jonathan J <n.jonathan.jiang@gmail.com>
2023-05-15 08:05:14 -0500

    .

--- compiler/vm/RAM.java
+++ compiler/vm/RAM.java
@@ -1,51 +1,43 @@
 package compiler.vm;
 
-import java.nio.ByteOrder;
-import java.util.Arrays;
-
 public class RAM {
-    private static final ByteOrder BYTE_ORDER = ByteOrder.LITTLE_ENDIAN;
-
     private Memory stack, heap;
 
-    private static class Memory {
-        private static final int MAX_MEMORY = 1 << 24;
-
-        private byte[] memory = new byte[65536];
-        private int used = 0;
-
-        private byte get(int i) {
-            if(i < 0)
-                throw new Error("JeVM error: Cannot access negative memory");
-            if(i >= MAX_MEMORY)
-                throw new Error("JeVM error: Out of memory (" + i + " exceeds the maximum allowed memory of " + MAX_MEMORY + " bytes)");
-
-            while (i >= used){
-                used *= 2;
-                memory = Arrays.copyOf(memory, used);
-            }
-
-            return memory[i];
-        }
+    private byte _get(int ptr){
+        return ptr < 0 ? heap.get(-(ptr + 1)) : stack.get(ptr);
+    }
+    private void _set(int ptr, byte value){
+        if (ptr < 0)
+            heap.set(-(ptr + 1), value);
+        else
+            stack.set(ptr, value);
     }
 
-    private byte _get(int i){
-        return i < 0 ? heap.get(-(i + 1)) : stack.get(i);
+    public char getChar(int ptr){
+        return (char) (_get(ptr) & 0xFF);
     }
-
-    public byte getByte(int i){
-        return _get(i);
+    public void setChar(int ptr, char c){
+        _set(ptr, (byte)(c & 0xFF));
     }
-    public char getChar(int i){
-        return (char) (_get(i) & 0xFF);
+    public boolean getBool(int ptr){
+        return _get(ptr) != 0;
     }
-    public boolean getBool(int i){
-        return _get(i) != 0;
+    public void setBool(int ptr, boolean b) {
+        _set(ptr, (byte)(b ? 1 : 0));
     }
-    public int getInt(int i){
-        return _get(i) | _get(i + 1) << 8 | _get(i + 2) << 16 | _get(i + 3) << 24;
+    public int getInt(int ptr){
+        return _get(ptr) | _get(ptr + 1) << 8 | _get(ptr + 2) << 16 | _get(ptr + 3) << 24;
     }
-    public float getFloat(int i){
-        return Float.intBitsToFloat(_get(i) | _get(i + 1) << 8 | _get(i + 2) << 16 | _get(i + 3) << 24);
+    public void setInt(int ptr, int val) {
+        _set(ptr, (byte)(val & 0xFF));
+        _set(ptr + 1, (byte)(val >> 8 & 0xFF));
+        _set(ptr + 2, (byte)(val >> 16 & 0xFF));
+        _set(ptr + 3, (byte)(val >> 24 & 0xFF));
+    }
+    public float getFloat(int ptr){
+        return Float.intBitsToFloat(getInt(ptr));
+    }
+    public void setFloat(int ptr, float val) {
+        setInt(ptr, Float.floatToRawIntBits(val));
     }
 }
